fd9d20dd21cd5ea3c431879fe5acb682a032408c
hammmu <104701130+hammmu@users.noreply.github.com>
2023-10-02 02:39:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,47 +3,35 @@ from isbntools.app import *
 import json
 from flask_cors import CORS
 app = Flask(__name__)
-
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 CORS(app,origins='*') 
 
 
-def get_book_metadata(isbn):
-    metadata = meta(isbn)
-    print(metadata)
-    return metadata['Title']
+@app.route('/upload', methods=['POST'])
+def getBookDetails():
+        json=request.get_json()
+        barcodes = json['barcode']
+        for barcode in barcodes:
+                metadata = meta(barcode)
+                author_name = metadata['Authors']
+                book_name = metadata['Title']
+                scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
+                        "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+                credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+                client = gspread.authorize(credentials)
+                # Open the Google Sheets document
+                sheet = client.open_by_key('18R1iIEU8BoeWYREOvl879jcQriH8zHFLt9P6FbJ4Sks').sheet1
+                # Convert the values to strings explicitly
+                barcode_str = str(barcode)
+                book_name_str = str(book_name)
+                author_name_str = str(author_name)
 
-
-@app.route('/getBookData',methods=['POST'])
-def hello():
-    print('i am called')
-    barcodes=request.get_json()
-    print(barcodes)
-    temp=[]
-    for value in barcodes['barcodes']:
-        temp.append(get_book_metadata(str(value)))
-    return temp
-
-@app.route('/')
-def home():
-	html = """
-<html>
- <head>
-  <title>
-   Google Cloud Run - Sample Python Flask Example
-  </title>
- </head>
- <body>
-  <p>Hello Hyder</p>
- </body>
-</html>
-"""
-	return html
+                # Append the row to the sheet
+                sheet.append_row([barcode_str, book_name_str, author_name_str])
+        return [author_name,book_name]
     
+
+# Run the Flask application
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-# # Access data
-# data = sheet.get_all_records()
-# print(data)
+    app.run(host='0.0.0.0',debug=True)
