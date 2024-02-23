ba530b81eca15ffb9b3efc3c8fc8844c7436e917
hammmu <104701130+hammmu@users.noreply.github.com>
2024-02-23 19:02:01 +0500

    Create tester.py

---
+++ tester.py
@@ -0,0 +1,16 @@
+import pymssql
+
+# Establishing connection to the MySQL database
+try:
+    # Replace 'username', 'password', 'hostname', 'database_name' with your credentials
+    conn = pymssql.connect(server="209.134.48.123", user="SA",
+                                    password="9ypybzq97ylVPVsdhQRw", database="HomeHealthStaging", as_dict=True)
+    print("Connected to MySQL database successfully")
+
+    # Perform operations here...
+
+    # Closing the connection
+    conn.close()
+    print("Connection closed")
+except pymssql.Error as error:
+    print("Failed to connect to MySQL: {}".format(error))
